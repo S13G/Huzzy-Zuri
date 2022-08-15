3be2568758275899c4bfb7d50dd7783687ad93b5
s13g <ayflix0@gmail.com>
2022-08-15 18:09:34 +0100

    ...

--- NewProject40/settings/common.py
+++ NewProject40/settings/common.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django_filters',
     'djoser',
     'jazzmin',
-    'social_django',
+    # 'social_django',
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
